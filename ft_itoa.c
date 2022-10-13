d257581a4fe8153c982d50d59bee4fc2ed77f979
Mohcine-Ghalmi <mohcin.ghalmi@gmail.com>
2022-10-13 12:20:03 +0100

    commit

--- ft_itoa.c
+++ ft_itoa.c
@@ -12,7 +12,42 @@
 
 #include "libft.h"
 
+int	count(int n)
+{
+	int	somme;
+
+	somme = 2;
+	if (n < 0)
+		somme++;
+	while (n / 10)
+	{
+		somme++;
+		n = n / 10;
+	}
+	return (somme);
+}
+
 char	*ft_itoa(int n)
 {
-	
+	char	*str;
+	int		i;
+	int		b;
+
+	i = 0;
+	b = count(n);
+	if (n < 0)
+		n *= -1;
+	str = malloc(sizeof(char) * b);
+	if (!str)
+		return (NULL);
+	str[--b] = '\0';
+	while (n / 10)
+	{
+		str[--b] = (n % 10) + '0';
+		n = n / 10;
+	}
+	str[--b] = n % 10 + '0';
+	if (b == 1)
+		str[--b] = '-';
+	return (str);
 }
