f9927007c3ead0255ef0e809ad271523b2a114f6
Mohcine-Ghalmi <mohcin.ghalmi@gmail.com>
2022-10-14 17:00:56 +0100

    commit

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: mghalmi <mghalmi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 11:30:47 by mghalmi           #+#    #+#             */
-/*   Updated: 2022/10/14 15:30:40 by mghalmi          ###   ########.fr       */
+/*   Updated: 2022/10/14 17:00:20 by mghalmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,19 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	size_t  len;
-    size_t  i;
-    char    *str;
+	size_t len;
+    int i;
+    char *str;
 
     i = 0;
     if (!s || !f)
-        return (0);
+        return (NULL);
     len = ft_strlen(s);
     str = malloc(sizeof(char) * (len + 1));
-        return (0);
     while (i < len)
     {
         str[i] = f(i, s[i]);
         i++;
     }
-    str[i] = '\0';
     return (str);
-}
-
-int mian()
-{
-	
 }
